5e3b79b47164e7b4577bc146d30a12e213e59476
remylian <137064504+remylian@users.noreply.github.com>
2025-04-15 22:01:42 +0200

    first draft for updating post

--- src/js/router/views/postEdit.js
+++ src/js/router/views/postEdit.js
@@ -1,3 +1,9 @@
-import { authGuard } from "../../utilities/authGuard";
+import { onUpdatePost } from '../../ui/post/update';
+import { authGuard } from '../../utilities/authGuard';
 
 authGuard();
+
+const form = document.forms.editPost;
+if (form) {
+	form.addEventListener('submit', onUpdatePost);
+}
