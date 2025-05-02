a7a7ba544b74bad85153c61eb16b6a770250aa88
remylian <137064504+remylian@users.noreply.github.com>
2025-05-02 23:15:28 +0200

    fixed issue deleting post from single post view

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,6 +1,6 @@
 import { authGuard } from '../../utilities/authGuard';
 import { readPost } from '../../api/post/read';
-import { onDeletePost } from '../../ui/post/delete';
+import { deletePost } from '../../api/post/delete';
 import { onLogout } from '../../ui/global/logout';
 
 authGuard();
@@ -53,10 +53,14 @@ authGuard();
 
 		// If this is my post, hook up the delete button
 		if (isMyPost) {
-			container.querySelector('#delete-button').addEventListener('click', async () => {
+			container.querySelector('#delete-button').addEventListener('click', async (e) => {
+				e.preventDefault();
+				if (!confirm('Are you sure you want to delete this post?')) return;
+
 				try {
-					// onDeletePost expects an event-like object; here, we simulate an event.
-					await onDeletePost({ target: { getAttribute: () => post.id } });
+					await deletePost(post.id);
+
+					window.location.href = '/';
 				} catch (e) {
 					console.error(e);
 					alert('Failed to delete post');
