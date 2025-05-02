a0928b48b6132832b07244ce03e469befeae6328
remylian <137064504+remylian@users.noreply.github.com>
2025-05-02 12:51:38 +0200

    no changes, just spacing

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -3,34 +3,45 @@
 // You may change the behaviour or approach of this file if you choose
 
 const BASE = import.meta.env.BASE_URL;
+console.log({ BASE, rawpath: window.location.pathname });
 
 export default async function router(rawpath = window.location.pathname) {
 	const pathname = rawpath.replace(BASE, '/');
+	console.log('→ routing to', pathname);
+
 	switch (pathname) {
 		case '/':
 			await import('./views/home.js');
 			break;
+
 		case '/auth/':
 			await import('./views/auth.js');
 			break;
+
 		case '/auth/login/':
 			await import('./views/login.js');
 			break;
+
 		case '/auth/register/':
 			await import('./views/register.js');
 			break;
+
 		case '/post/':
 			await import('./views/post.js');
 			break;
+
 		case '/post/edit/':
 			await import('./views/postEdit.js');
 			break;
+
 		case '/post/create/':
 			await import('./views/postCreate.js');
 			break;
+
 		case '/profile/':
 			await import('./views/profile.js');
 			break;
+
 		default:
 			await import('./views/notFound.js');
 	}
