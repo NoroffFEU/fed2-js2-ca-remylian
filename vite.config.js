0cc9dcdc8733ca00e447518102d4d67fb2f9a0aa
remylian <137064504+remylian@users.noreply.github.com>
2025-05-02 12:15:27 +0200

    vite output to docs folder for gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,20 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	appType: 'mpa',
 	base: '/fed2-js2-ca-remylian/',
 	build: {
+		outDir: 'docs',
 		target: 'esnext',
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, './index.html'),
-				login: resolve(__dirname, './auth/login/index.html'),
 				auth: resolve(__dirname, './auth/index.html'),
-				register: resolve(__dirname, './auth/register/index.html'),
+				'auth/login': resolve(__dirname, './auth/login/index.html'),
+				'auth/register': resolve(__dirname, './auth/register/index.html'),
 				profile: resolve(__dirname, './profile/index.html'),
 				post: resolve(__dirname, './post/index.html'),
-				editPost: resolve(__dirname, './post/edit/index.html'),
-				createPost: resolve(__dirname, './post/create/index.html'),
+				'post/create': resolve(__dirname, './post/create/index.html'),
+				'post/edit': resolve(__dirname, './post/edit/index.html'),
 			},
 		},
 	},
