c7a23d07bebcc3a4cf7bbd3d1c9639332f7ba198
remylian <137064504+remylian@users.noreply.github.com>
2025-05-02 13:28:10 +0200

    updating build

---
+++ docs/assets/authGuard-D1nHpxub.js
@@ -0,0 +1,1 @@
+function a(){const e="/fed2-js2-ca-remylian/";localStorage.getItem("accessToken")||(alert("You must be logged in to view this page"),window.location.href=`${e}auth/login/`)}export{a};
--- docs/assets/authGuard-DfzkPADv.js
+++
@@ -1,1 +0,0 @@
-function e(){localStorage.getItem("accessToken")||(alert("You must be logged in to view this page"),window.location.href="/auth/login/")}export{e as a};
